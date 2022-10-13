a411d8ebf094835f80e8743916c6c7be301899ed
Uriel Hernandez <urielHernandezgarcia1@gmail.com>
2022-10-13 16:00:54 -0500

    commit3

---
+++ js/index.js
@@ -0,0 +1,26 @@
+// decalrara variables para vincular los objetso de formulario
+const usuario = document.getElementById('user');
+const password = document.getElementById('pass');
+const formulario = document.getElementById('formLogin');
+
+//Genrando eventos
+formulario.addEventListener('submit', login);
+
+//Funciones a ejecutar
+function login(e){
+e.preventDefault();
+
+let usuarioVal = usuario.value;
+let passwordVal = password.value;
+
+if(usuarioVal == '' || passwordVal == ''){
+    return;
+}
+
+console.log('el valor para user es: '+ usuarioVal);
+console.log('el valor para pass es: '+ passwordVal);
+
+
+
+
+}
