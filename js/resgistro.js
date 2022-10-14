8ff77afcd7aa4475f8b288f3026b28490a504e06
Uriel Hernandez <urielHernandezgarcia1@gmail.com>
2022-10-13 20:42:17 -0500

    "Guardando usuario"

--- js/resgistro.js
+++ js/resgistro.js
@@ -18,5 +18,18 @@ function registro(e){
     if(nombreVal =='' || emailVal == '' || userVal == '' || passValue == ''){
         return;
     }
-    console.log('validado datos!');
+    const usuario = {
+        nombre: nombreVal,
+        email: emailVal,
+        user: userVal,
+        pass: passValue,
+    }
+    localStorage.setItem('usuario', JSON.stringify(usuario));
+
+    nombre.value = '';
+    email.value = '';
+    user.value = '';
+    pass.value = '';
+
+    console.log('usuario guardado');
 }
