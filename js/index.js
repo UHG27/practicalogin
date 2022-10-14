4b76c6f3214234b140d4a49c24bf152f5d0209b1
Uriel Hernandez <urielHernandezgarcia1@gmail.com>
2022-10-13 21:57:15 -0500

    "validando login"

--- js/index.js
+++ js/index.js
@@ -14,13 +14,20 @@ let usuarioVal = usuario.value;
 let passwordVal = password.value;
 
 if(usuarioVal == '' || passwordVal == ''){
+    creaMensaje('Verifica tus campos','danger');
     return;
 }
 
-console.log('el valor para user es: '+ usuarioVal);
-console.log('el valor para pass es: '+ passwordVal);
+if(localStorage.getItem('usuario')){
+    let objeto = JSON.parse(localStorage.getItem('usuario'));
 
-
-
+    if( usuarioVal == objeto.user && passwordVal == objeto.pass){
+        creaMensaje('Login correcto','success');
+    }else{
+        creaMensaje('Uusaurio Incorrecto','danger');
+    }
+}else{
+    creaMensaje('No hay registros','danger');
+}
 
 }
